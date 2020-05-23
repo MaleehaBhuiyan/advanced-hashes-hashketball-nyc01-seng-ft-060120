bc960abb854759e4aec18aa2178072c551728530
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-23 23:12:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,6 +191,7 @@ def big_shoe_rebounds
     shoe_array << player[:shoe]
     rebound_array << player[:rebounds]
     shoe_rebound_hash = Hash[rebound_array.zip(shoe_array)]
-    binding.pry
   end
+  return shoe_rebound_hash
 end
+binding.pry 
