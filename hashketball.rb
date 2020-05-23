cb70ee1924c9eec90824596d4d8945d4ec8faa9f
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-23 22:18:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,11 +166,11 @@ def player_numbers(team)
   game_hash.each do |location, team_info|
     if team == team_info[:team_name]
       team_info[:players].each do |player|
-        return jersey_array << player[:number]
-        binding.pry
+        #return jersey_array << player[:number]
       end
     end
   end
+  return jersey_array << player[:number]
 end
 
 def player_stats(name)
