3094f45c9568f379216a26c90253fc011049843f
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-23 20:14:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,7 +151,7 @@ end
 
 def team_colors(team)
   game_hash.each do |location, team_info|
-    team_info[:team_name]
-  binding.pry
-end
+    if team == team_info[:team_name]
+      return team_info[:colors]
+    end
 end
