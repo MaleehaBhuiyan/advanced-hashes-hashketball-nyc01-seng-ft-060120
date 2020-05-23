70c35c9fef631cf136a1246b03c77afff7e1bd59
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-23 19:25:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,6 +129,6 @@ def game_hash
 end
 
 def num_points_scored(name)
-  player = game_hash[:home][:players].merge(game_hash[:away][:players])
+  game_hash[:home][:players].merge(game_hash[:away][:players])
   binding.pry
 end
